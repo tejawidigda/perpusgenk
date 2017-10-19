8532e6da889cc59218788aefaf92bf880d5a8c17
tejawidigda <32919497+tejawidigda@users.noreply.github.com>
2017-10-19 10:46:39 +0700

    Update bot-api-config.php

--- bot-api-config.php
+++ bot-api-config.php
@@ -4,7 +4,7 @@ if (!defined('HS')) {
     die('Tidak boleh diakses langsung.');
 }
 
-$token = '';
+$token = '461313707:AAEayNWa3H1NFaU15HubxndlustkZW4rPhE';
 $debug = true;
 
-$chatid = '213567634';
+$chatid = '188216146';
